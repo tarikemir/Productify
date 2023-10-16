67728b9facd6af59b20be92d568c18176e2d26d1
tarikemir <81103446+tarikemir@users.noreply.github.com>
2023-10-16 19:30:44 +0300

    Add project files.

---
+++ Core/Productify.Domain/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productify.Domain.Entities
+{
+    public class Product
+    {
+        public Product( string title)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+        }
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ Presentation/Productify.MVC/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Productify.Domain.Entities;
+using Productify.Persistence.Contexts;
+
+namespace Productify.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductifyDbContext _context;
+
+        public ProductController()
+        {
+            _context = new();
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return View(_context.Products.ToList());
+        }
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(string productName)
+        {
+            _context.Products.Add(new Product(productName));
+            _context.SaveChanges();
+            return View();
+        }
+        public IActionResult GetProduct()
+        {
+            return View();
+        }
+    }
+}
